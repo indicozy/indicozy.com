d40008ddd87b2ece21e04993d3617a8b35bb74a2
indicozy <berkutkarlibai@gmail.com>
2023-04-23 17:26:10 +0600

    added cva

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+        com: ["Commissioner"],
+      },
+      colors: {
+        bg: "#f5f2ea",
+        cblue: "#1c295e",
+        ccyan: "#4e87a1",
+        cgreen: "#989635",
+        cyellow: "#fcb426",
+        cred: "#ec1c2e",
+      },
+    },
+  },
+  plugins: [],
+};
