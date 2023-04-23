29cd1ae0eb583998f1ffd0bbf0630c71dd14afc2
indicozy <berkutkarlibai@gmail.com>
2023-04-23 16:15:05 +0600

    added cva

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), partytown()]
+  site: "https://indicozy.com",
+  integrations: [mdx(), sitemap(), partytown(), tailwind()],
 });
